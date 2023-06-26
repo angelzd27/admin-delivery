5ce9acfd79122f73b00060d1c178c1e28ba3d3a6
angelzd27 <angeltj27@gmail.com>
2023-06-26 13:50:06 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-3xl font-bold">
+        <h1>Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
