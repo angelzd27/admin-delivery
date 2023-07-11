b9ac45b070c8278c904401376c5c63923546c2af
EdwinReyes009 <reyesmorales56@gmail.com>
2023-07-10 23:02:19 -0600

    Login layout

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import Form from '../components/Form'
+
+function Login() {
+    return (
+        <>
+            <div>
+                <Form />
+            </div>
+        </>
+    )
+}
+
+export default Login
