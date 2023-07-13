f63546a78ba507af6c54e81023dd05ae6086e688
DeicideSuici <eduardx62@outlook.com>
2023-07-13 11:56:33 -0600

    Agregar dependencia de iconos y sidebar terminado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { MdDashboard, MdSettings, MdAdminPanelSettings, MdPerson, MdHelp } from 'react-icons/md'
 
 function Sidebar() {
     const [showMenu, setShowMenu] = useState(false)
@@ -23,49 +24,48 @@ function Sidebar() {
             <div className={`bg-black text-white h-full fixed lg:static w-[80%] transition-all duration-400 ${showMenu ? 'left-0' : '-left-full'}`}>
 
                 {/* Profile Info */}
-                <div className='flex flex-col items-center justify-center p8 gap-2 h-[30vh]'>
+                <div className='flex flex-col items-center justify-center p-8 gap-3 h-[30vh]'>
                     <img
-                        className='w-20 h-20 object-cover rounded-full'
+                        className='w-32 h-32 object-cover rounded-full'
                         src={image_url} />
                     <h1
-                        className='text-xl font-bold'>
-                        @DeicideSuici
+                        className='text-2xl font-bold flex items-center gap-2'>
+                        <MdPerson />
+                        DeicideSuici
                     </h1>
 
                     {/* Nota: Cambiar el color de la etiqueta Spam una vez quse se haya decidido el color */}
                     <span
-                        className='bg-orange-400 py-1 px-3 rounded-full'>
+                        className='bg-yummy-800 py-1 px-3 rounded-full flex items-center gap-2'>
+                        <MdAdminPanelSettings />
                         Admin Level
                     </span>
                 </div>
 
                 {/* Navigation */}
-                <div className='bg-orange-400 p-8 rounded-tr-[100px] h-[70vh] flex flex-col justify-between gap-8'>
+                <div className='bg-yummy-800 p-8 rounded-tr-[100px] h-[70vh] flex flex-col justify-between gap-8'>
                     <nav className='flex flex-col gap-6'>
                         <a
                             href=''
-                            className='flex items-center gap-4 text-white py-2 px-4 rounded-xl hover:bg-orange-600 transition-colors'>
-                            Home
+                            className='flex items-center gap-4 text-white py-2 px-4 rounded-xl hover:bg-yummy-600 transition-colors'>
+                            <MdDashboard />
+                            Dashboard
                         </a>
                         <a
                             href=''
-                            className='flex items-center gap-4 text-white py-2 px-4 rounded-xl hover:bg-orange-600 transition-colors'>
-                            Link
-                        </a>
-                        <a
-                            href=''
-                            className='flex items-center gap-4 text-white py-2 px-4 rounded-xl hover:bg-orange-600 transition-colors'>
-                            Link
-                        </a>
-                        <a
-                            href=''
-                            className='flex items-center gap-4 text-white py-2 px-4 rounded-xl hover:bg-orange-600 transition-colors'>
-                            Link
+                            className='flex items-center gap-4 text-white py-2 px-4 rounded-xl hover:bg-yummy-600 transition-colors'>
+                            <MdSettings />
+                            Settings
                         </a>
                     </nav>
-                    <div className='bg-orange-300 text-black p-4 rounded-xl'>
-                        <p className='text-sm'>Having troubles?</p>
-                        <a href='' className='font-bold text-md'>Contact Us</a>
+                    <div className='bg-yummy-600 text-black p-4 rounded-xl flex flex-row gap-4 items-center'>
+                        <div>
+                            <MdHelp className='w-5 h-5' />
+                        </div>
+                        <div>
+                            <p className='text-sm mb-2'>Having troubles?</p>
+                            <a href='' className='font-bold text-md flex flex-row gap-2 items-center underline underline-offset-3'>Contact Us !</a>
+                        </div>
                     </div>
                 </div>
             </div>
