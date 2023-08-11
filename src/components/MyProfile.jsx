491814cd220844648bfaba9552651962bfd2df45
DeicideSuici <eduardx62@outlook.com>
2023-08-11 10:15:32 -0600

    Sidebar Finally Fixed !

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,11 +1,11 @@
-function Settings() {
+function MyProfile() {
     return (
         <>
             <div className='flex flex-col gap-6'>
-                <h1 className='text-2xl'>Settings</h1>
+                <h1 className='text-2xl'>My Profile</h1>
             </div>
         </>
     )
 }
 
-export default Settings
+export default MyProfile
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Sidebar from '../components/Sidebar'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import Dashboard from '../components/Dashboard'
-import Settings from '../components/Settings'
+import MyProfile from '../components/MyProfile'
 import Dishes from '../components/Dishes'
 
 function Home() {
@@ -10,12 +10,12 @@ function Home() {
         <>
             <div className='grid grid-cols-6 min-h-screen overflow-y-scroll bg-slate-50'>
                 <Sidebar />
-                <div className='mt-10 col-span-6 lg:col-span-5 mr-[4.5%]'>
+                <div className='mt-10 col-span-6 xl:col-span-5 xl:col-start-2 mr-[4.5%]'>
                     <Routes>
                         <Route path='/' element={<Navigate to='/home/dashboard' />} />
                         <Route path='/dishes' element={<Dishes />} />
                         <Route path='/dashboard' element={<Dashboard />} />
-                        <Route path='/settings' element={<Settings />} />
+                        <Route path='/my_profile' element={<MyProfile />} />
                         <Route path='/*' element={<Navigate to='/not-found' />} />
                     </Routes>
                 </div>
