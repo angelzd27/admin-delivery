f91e5896248ae86a23deb04154e08d97cfb7d9df
RodrigoLa04 <rodrigolabrada98@gmail.com>
2023-08-11 22:54:03 -0600

    dashboard_dishes_1

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -1,11 +1,49 @@
+import React from 'react';
+
 function Dishes() {
+    const dishesData = [
+        {
+            name: "Platillo 1",
+            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
+        },
+        {
+            name: "Platillo 2",
+            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
+        },
+        {
+            name: "Platillo 3",
+            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
+        },
+        {
+            name: "Platillo 4",
+            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
+        },
+        {
+            name: "Platillo 5",
+            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
+        },
+        {
+            name: "Platillo 6",
+            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
+        },
+     
+    ];
+
     return (
-        <>
-            <div className='flex flex-col gap-6'>
-                <h1 className='text-2xl'>Dishes</h1>
+        <div className='flex flex-col items-center gap-6'>
+            <h1 className='text-2xl text-yummy-800 text-center'>Dishes</h1>
+            <div className='w-4/5'>
+                {dishesData.map((dish, index) => (
+                    <div key={index} className='border p-4 flex items-center mb-6'>
+                        <img src={dish.photoUrl} alt={dish.name} className='w-24 h-auto mr-4' />
+                        <a href={`#${dish.name}`} className='text-blue-500 hover:underline'>
+                            <h2 className='text-xl'>{dish.name}</h2>
+                        </a>
+                    </div>
+                ))}
             </div>
-        </>
-    )
+        </div>
+    );
 }
 
-export default Dishes
+export default Dishes;
