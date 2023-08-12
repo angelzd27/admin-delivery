d9ec804a3f6e95291113a1cb04a49a441fb1d59f
RodrigoLa04 <rodrigolabrada98@gmail.com>
2023-08-11 23:05:59 -0600

    dishes con Mui

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 function Dishes() {
     const dishesData = [
@@ -11,35 +16,49 @@ function Dishes() {
             photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
         },
         {
-            name: "Platillo 3",
+            name: "Platillo 2",
             photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
         },
         {
-            name: "Platillo 4",
+            name: "Platillo 2",
             photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
         },
         {
-            name: "Platillo 5",
+            name: "Platillo 2",
+            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
+        },{
+            name: "Platillo 2",
             photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
         },
-        {
-            name: "Platillo 6",
-            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
-        },
-     
+        
     ];
 
     return (
         <div className='flex flex-col items-center gap-6'>
-            <h1 className='text-2xl text-yummy-800 text-center'>Dishes</h1>
+            <Typography variant='h4' color='primary' align='center'>
+                Dishes
+            </Typography>
             <div className='w-4/5'>
                 {dishesData.map((dish, index) => (
-                    <div key={index} className='border p-4 flex items-center mb-6'>
-                        <img src={dish.photoUrl} alt={dish.name} className='w-24 h-auto mr-4' />
-                        <a href={`#${dish.name}`} className='text-blue-500 hover:underline'>
-                            <h2 className='text-xl'>{dish.name}</h2>
-                        </a>
-                    </div>
+                    <Card key={index} className='mb-6'>
+                        <CardActionArea>
+                            <div className='flex'>
+                                <CardMedia
+                                    component='img'
+                                    style={{ width: '100px', height: '100px' }} 
+                                    image={dish.photoUrl}
+                                    alt={dish.name}
+                                />
+                                <CardContent className='flex items-center'>
+                                    <Typography gutterBottom variant='h5' component='div'>
+                                        <a href={`#${dish.name}`} className='text-blue-500 hover:underline'>
+                                            {dish.name}
+                                        </a>
+                                    </Typography>
+                                </CardContent>
+                            </div>
+                        </CardActionArea>
+                    </Card>
                 ))}
             </div>
         </div>
