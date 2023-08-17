c18b214a4235a79ba71537eef291b1630dca160a
RodrigoLa04 <rodrigolabrada98@gmail.com>
2023-08-17 15:04:07 -0600

    dishesNuevo

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -1,57 +1,126 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import React, { useState } from 'react';
+import { TextField, Card, CardContent, Typography, Accordion, AccordionSummary, AccordionDetails, Button } from '@mui/material'; 
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import burgerIcon from '../assets/icons/burger.png'; 
+import pastaIcon from '../assets/icons/pasta.png'; 
+import pizzaIcon from '../assets/icons/pizza.png'; 
+import steakIcon from '../assets/icons/steak.png'; 
+import SearchIcon from '@mui/icons-material/Search';
 
-function Dishes() {
-    const dishesData = [
-        {
-            name: "Platillo 1",
-            description: "Descripción del platillo 1",
-            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
-        },
-        {
-            name: "Platillo 2",
-            description: "Descripción del platillo 2",
-            photoUrl: "https://assets.kraftfoods.com/recipe_images/opendeploy/118961_640x428.jpg",
-        },
-        
-    ];
+const categories = [
+  { name: "Hamburguesa", icon: burgerIcon }, 
+  { name: "Pizza", icon: pizzaIcon },  
+  { name: "Carne", icon: steakIcon}, 
+  { name: "Pasta", icon: pastaIcon }, 
+ 
+];
 
-    return (
-        <div className='flex flex-col items-center gap-6'>
-            <Typography variant='h4' color='primary' align='center'>
-                Dishes
-            </Typography>
-            <div className='w-full'>
-                {dishesData.map((dish, index) => (
-                    <Card key={index} className='mb-6 rounded-lg'>
-                        <CardActionArea>
-                            <div className='flex'>
-                                <CardContent className='flex flex-col'>
-                                    <Typography gutterBottom variant='h5' component='div'>
-                                        {dish.name}
-                                    </Typography>
-                                    <Typography variant='body2' color='textSecondary'>
-                                        {dish.description}
-                                    </Typography>
-                                </CardContent>
-                                <div className='flex-grow'></div>
-                                <CardMedia
-                                    component='img'
-                                    style={{ width: '100px', height: '100px' }} // Ajusta estos valores
-                                    image={dish.photoUrl}
-                                    alt={dish.name}
-                                />
-                            </div>
-                        </CardActionArea>
-                    </Card>
-                ))}
-            </div>
+
+
+const dishes = [
+    {
+      id: 1,
+      title: "Hamburguesa Clásica",
+      image: "https://thefoodtech.com/wp-content/uploads/2023/05/sabrosa-hamburguesa-papas-fritas.jpg",
+      description: "Una deliciosa hamburguesa con ingredientes frescos y sabrosos."
+    },
+    {
+      id: 2,
+      title: "Pizza Margarita",
+      image: "https://assets.unileversolutions.com/recipes-v2/244028.jpg",
+      description: "Una pizza clásica con tomate, mozzarella y albahaca."
+    },
+    {
+        id: 3,
+        title: "Sushi",
+        image: "https://assets.unileversolutions.com/recipes-v2/244028.jpg",
+        description: "Una pizza clásica con tomate, mozzarella y albahaca."
+    },
+    {
+        id: 4,
+        title: "Elote",
+        image: "https://assets.unileversolutions.com/recipes-v2/244028.jpg",
+        description: "Una pizza clásica con tomate, mozzarella y albahaca."
+    },
+    {
+        id: 5,
+        title: "Nachos",
+        image: "https://assets.unileversolutions.com/recipes-v2/244028.jpg",
+        description: "Una pizza clásica con tomate, mozzarella y albahaca."
+    },
+    {
+        id: 6,
+        title: "Nuggets",
+        image: "https://assets.unileversolutions.com/recipes-v2/244028.jpg",
+        description: "Una pizza clásica con tomate, mozzarella y albahaca."
+    },
+
+   
+  ];
+
+const MenuComponent = () => {
+  const [expandedDish, setExpandedDish] = useState(null);
+
+  const handleExpand = (dishId) => {
+    if (expandedDish === dishId) {
+      setExpandedDish(null);
+    } else {
+      setExpandedDish(dishId);
+    }
+  };
+
+  return (
+    <div className="bg-ff6d75 min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-4xl bg-white rounded-lg p-4">
+        <div className="flex items-center mb-8">
+          <Typography variant="h4" className="mr-4">
+            <span className="font-semibold">Ordena</span> algo
+          </Typography>
+          <div className="flex-grow rounded-full overflow-hidden border border-gray-300 flex">
+            <TextField className="flex-grow px-2" variant="outlined" placeholder="Busca algo" />
+            <button className="p-2 bg-gray-200">
+              <SearchIcon />
+            </button>
+          </div>
         </div>
-    );
-}
+        <Typography variant="h5" className="mb-4">
+          Platillos populares
+        </Typography>
+        <div className="grid grid-cols-4 gap-4 mb-4">
+          {categories.map((category, index) => (
+            <div key={index} className="rounded-lg overflow-hidden bg-gray-100 p-3 hover:bg-red-200 transition duration-300 border border-red-400">
+              <div className="flex flex-col items-center text-center">
+                <img src={category.icon} alt={category.name} className="w-6 h-6 mb-2" />
+                <span>{category.name}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <Typography variant="h5" className="mb-4">
+          Platillos populares
+        </Typography>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+          {dishes.map((dish) => (
+            <Card key={dish.id} variant="outlined">
+              <CardContent>
+                <img src={dish.image} alt={dish.title} className="w-full mb-2 rounded-md" />
+                <Typography variant="h6">{dish.title}</Typography>
+                
+                <Accordion expanded={expandedDish === dish.id}>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />} onClick={() => handleExpand(dish.id)} aria-controls={`dish-description-${dish.id}`} id={`dish-description-${dish.id}`}>
+                    Ver más
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>{dish.description}</Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dishes;
+export default MenuComponent;
