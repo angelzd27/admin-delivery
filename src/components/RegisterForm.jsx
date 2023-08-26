2aea99b80c2991e423a2f48fbee180eebd7856e8
DeicideSuici <eduardx62@outlook.com>
2023-08-26 17:31:05 -0600

    Register Form

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,121 +1,78 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { Link, useNavigate } from 'react-router-dom'
 import { TextField } from '@mui/material'
 import MenuItem from '@mui/material/MenuItem'
+import { BiArrowFromBottom } from 'react-icons/bi'
+import Loader from './Loader'
 import { countries } from '../services/country_codes'
 import { gender } from '../services/gender'
 import { profile } from '../services/profile'
 import SignUp from '../assets/animations/SignUp.mp4'
+import { BD_ACTION_POST } from '../services/master'
 
 function RegisterForm() {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    secondLastName: '',
+  const navigate = useNavigate()
+  const [load, setLoad] = useState(false)
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+    type_user_id: 1,
+    first_name: '',
+    last_name: '',
+    second_last_name: '',
     phone: '',
     country: {},
-    gender: '',
-    profile: '',
-    username: '',
-    password: '',
-    confirmPassword: '',
-    email: '',
-    picture: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+    picture: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    id_gender: 1
   })
 
-  const [showError, setShowError] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('')
+  const upload_images = async (event) => {
+    const preset_key = 'lplxq1y3'
+    const cloud_name = 'du7kxnb4h'
 
-  function isValidEmail(email) {
-
-    // Expresión regular para validar el formato de un email
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    return emailRegex.test(email)
-
+    const file = event.target.files[0]
+    const formData = new FormData()
+    formData.append('file', file)
+    formData.append('upload_preset', preset_key)
+    const data = await axios.post(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, formData)
+    console.log(data)
+    setForm({ ...form, picture: data.data.secure_url })
   }
 
-  const handleRegister = () => {
-    // Realizar validaciones
-    const { email, password, confirmPassword } = formData
-
-    // Validar que todos los campos estén completos
-    if (Object.values(formData).some((value) => value === '')) {
-      setErrorMessage('Please complete all fields.')
-      setShowError(true)
-      return
-    } else {
-      setErrorMessage('')
-    }
-
-    // Validar campo email
-    if (!isValidEmail(email)) {
-      setErrorMessage('Please enter a valid email address.')
-      setShowError(true)
-      return
-    } else {
-      setShowError(false)
-    }
-
-    // Validar que las contraseñas coincidan
-    if (password !== confirmPassword) {
-      setErrorMessage('Passwords do not match.')
-      setShowError(true)
-      return
-    } else {
-      setShowError(false)
-    }
-
-    // Validar que la contraseña cumpla con los requisitos
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-    if (!passwordRegex.test(password)) {
-      setErrorMessage(
-        'Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one special character.'
-      )
-      setShowError(true)
-      return
-    }
-
-    setShowError(false)
-
-    // Si las validaciones son exitosas, aquí puedes guardar los datos en el backend
-    console.log('Formulario enviado:', formData)
-  }
-
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }))
-  }
-
-  const send = () => {
+  const post_new_user = async () => {
     const body = {
-      username: formData.username,
-      email: formData.email,
-      password: formData.password,
-      type_user_id: formData.profile,
-      first_name: formData.firstName,
-      last_name: formData.lastName,
-      second_last_name: formData.secondLastName,
-      phone: formData.phone,
-      iso_code: formData.country.code,
-      dial_code: formData.country.dial_code,
-      picture: formData.picture,
-      id_gender: formData.gender
+      username: form.username,
+      email: form.email,
+      password: form.password,
+      type_user_id: form.type_user_id,
+      first_name: form.first_name,
+      last_name: form.last_name,
+      second_last_name: form.second_last_name,
+      phone: form.phone,
+      iso_code: form.country.code,
+      dial_code: form.country.dial_code,
+      picture: form.picture,
+      id_gender: form.id_gender
     }
 
-    console.log(body)
-  }
+    const data = await BD_ACTION_POST('auth', 'sign_up', body)
 
-  const image = () => {
-    console.log('FUCK YOU')
+    if (!data.error) {
+      setLoad(true)
+      setTimeout(() => {
+        navigate(`/auth/sign_in`)
+      }, 2000)
+    } else {
+      setLoad(false)
+    }
   }
 
   return (
-
     <>
+      <Loader load={load} />
       <div className='bg-yummy-800 min-h-screen flex flex-row items-center relative justify-center h-full'>
 
         {/* Contenedor del formulario */}
@@ -126,8 +83,8 @@ function RegisterForm() {
               <TextField
                 label='First Name'
                 name='firstName'
-                value={formData.firstName}
-                onChange={handleInputChange}
+                value={form.first_name}
+                onChange={event => setForm({ ...form, first_name: event.target.value })}
                 variant='standard'
                 fullWidth
                 margin='normal'
@@ -138,8 +95,8 @@ function RegisterForm() {
                 <TextField
                   label='Last Name'
                   name='lastName'
-                  value={formData.lastName}
-                  onChange={handleInputChange}
+                  value={form.last_name}
+                  onChange={event => setForm({ ...form, last_name: event.target.value })}
                   variant='standard'
                   fullWidth
                   margin='normal'
@@ -149,8 +106,8 @@ function RegisterForm() {
                 <TextField
                   label='Second Last Name'
                   name='secondLastName'
-                  value={formData.secondLastName}
-                  onChange={handleInputChange}
+                  value={form.second_last_name}
+                  onChange={event => setForm({ ...form, second_last_name: event.target.value })}
                   variant='standard'
                   fullWidth
                   margin='normal'
@@ -163,8 +120,8 @@ function RegisterForm() {
                   <TextField
                     label='Phone'
                     name='phone'
-                    value={formData.phone}
-                    onChange={handleInputChange}
+                    value={form.phone}
+                    onChange={event => setForm({ ...form, phone: event.target.value })}
                     variant='standard'
                     fullWidth
                     margin='normal'
@@ -176,8 +133,8 @@ function RegisterForm() {
                     select
                     label='Country'
                     name='country'
-                    value={formData.country}
-                    onChange={handleInputChange}
+                    value={form.country}
+                    onChange={event => setForm({ ...form, country: event.target.value })}
                     variant='standard'
                     fullWidth
                     margin='normal'
@@ -198,8 +155,8 @@ function RegisterForm() {
                   select
                   label='Gender'
                   name='gender'
-                  value={formData.gender}
-                  onChange={handleInputChange}
+                  value={form.id_gender}
+                  onChange={event => setForm({ ...form, id_gender: event.target.value })}
                   variant='standard'
                   fullWidth
                   margin='normal'
@@ -217,8 +174,8 @@ function RegisterForm() {
                   select
                   label='Profile'
                   name='profile'
-                  value={formData.profile}
-                  onChange={handleInputChange}
+                  value={form.type_user_id}
+                  onChange={event => setForm({ ...form, type_user_id: event.target.value })}
                   variant='standard'
                   fullWidth
                   margin='normal'
@@ -235,21 +192,18 @@ function RegisterForm() {
             </div>
 
             <h2 className='text-gray-500 text-xl font-bold text-center mt-10 mb-5'>Account Information</h2>
-            <div className='flex flex-col items-center gap-4'>
-              <div className="relative w-36 h-36">
-                <div className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity bg-[rgba(0,0,0,0.6)] rounded-full cursor-pointer" onClick={() => { image() }}>
-                  <p className="text-white text-center text-sm select-none">Change Profile<br />Picture</p>
-                </div>
-                <img src={formData.picture} className='w-36 h-36 rounded-full select-none' />
-              </div>
+            <div className='flex flex-col items-center justify-center gap-4'>
+              <img src={form.picture} className='rounded-full shadow-lg w-[100px] h-[100px]' />
+              <input type='file' id='actual-btn' hidden onChange={upload_images} />
+              <label htmlFor='actual-btn' className='text-sky-500 flex items-center cursor-pointer justify-center gap-1 hover:bg-gray-100 px-4 py-2 rounded-xl hover:text-sky-400'>Choose File <BiArrowFromBottom /></label>
             </div>
             <div className='flex flex-row gap-6'>
               <TextField
                 label='Username'
                 name='username'
                 type='text'
-                value={formData.username}
-                onChange={handleInputChange}
+                value={form.username}
+                onChange={event => setForm({ ...form, username: event.target.value })}
                 variant='standard'
                 fullWidth
                 margin='normal'
@@ -260,8 +214,8 @@ function RegisterForm() {
                 label='Email'
                 name='email'
                 type='text'
-                value={formData.email}
-                onChange={handleInputChange}
+                value={form.email}
+                onChange={event => setForm({ ...form, email: event.target.value })}
                 variant='standard'
                 fullWidth
                 margin='normal'
@@ -274,8 +228,8 @@ function RegisterForm() {
                 label='Password'
                 name='password'
                 type='password'
-                value={formData.password}
-                onChange={handleInputChange}
+                value={form.password}
+                onChange={event => setForm({ ...form, password: event.target.value })}
                 variant='standard'
                 fullWidth
                 margin='normal'
@@ -287,8 +241,8 @@ function RegisterForm() {
                 label='Confirm Password'
                 name='confirmPassword'
                 type='password'
-                value={formData.confirmPassword}
-                onChange={handleInputChange}
+                value={form.confirm_password}
+                onChange={event => setForm({ ...form, confirm_password: event.target.value })}
                 variant='standard'
                 fullWidth
                 margin='normal'
@@ -299,21 +253,10 @@ function RegisterForm() {
               <Link to='/auth/sign_in' className='text-yummy-800 hover:text-yummy-600 transition-all duration-200'>
                 Return to Sign In
               </Link>
-              <Link onClick={() => send()} className='text-yummy-800 hover:text-yummy-600 transition-all duration-200'>
+              <Link onClick={() => post_new_user()} className='text-yummy-800 hover:text-yummy-600 transition-all duration-200'>
                 Register Now
               </Link>
             </div>
-
-            {showError && (
-              <div className='fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50 z-50'>
-                <div className='bg-white p-4 rounded-xl shadow-lg'>
-                  <p className='text-yummy-800 text-center'>{errorMessage}</p>
-                  <button onClick={() => setShowError(false)} className='mt-4 bg-yummy-800 text-white font-semibold py-2 px-4 rounded-xl'>
-                    Close
-                  </button>
-                </div>
-              </div>
-            )}
           </div>
         </div>
 
@@ -327,5 +270,6 @@ function RegisterForm() {
     </>
   )
 }
+
 
 export default RegisterForm
