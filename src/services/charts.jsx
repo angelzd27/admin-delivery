ace473928a206de5fd2f9bd089190f9b352613fa
DeicideSuici <eduardx62@outlook.com>
2023-12-11 04:04:15 -0600

    Ya ora si que chingue asumadre

--- src/services/charts.jsx
+++ src/services/charts.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const services = 'http://172.16.1.79:4003'
+const services = 'http://31.220.57.167:4004'
 
 export const data_chart = async (chart_type, chart_library) => {
     const url = `${services}/charts/${chart_type}`
